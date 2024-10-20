6bc2da68e83aa038b38c8acdeb31599541f4e091
chris <135366101+chris-gr81@users.noreply.github.com>
2024-10-20 20:43:34 +0200

    Debbug functionality

--- winzerWallet/main.lua
+++ winzerWallet/main.lua
@@ -3,67 +3,61 @@ import "Turbine.UI";
 import "Turbine.UI.Lotro";
 import "Turbine.Gameplay";
 
-mainBackClr = Turbine.UI.Color( 0, 0, 0);
+mainBackClr = Turbine.UI.Color( 0, 0, 0 );
 
-mainWindow = Turbine.UI.Window();
-mainWindow:SetVisible(true);
-mainWindow:SetSize(300, 40);
-mainWindow:SetPosition(900, 30);
-mainWindow:SetBackColor(mainBackClr);
-mainWindow:SetOpacity(0.6);
+local mainWindow = Turbine.UI.Window();
+mainWindow:SetVisible( true );
+mainWindow:SetSize( 300, 40 );
+mainWindow:SetPosition( 900, 30 );
+mainWindow:SetBackColor( mainBackClr );
+mainWindow:SetOpacity( 0.6 );
 
-targetLabel = Turbine.UI.Label();
-targetLabel:SetParent(mainWindow);
-targetLabel:SetPosition(10, 10);
-targetLabel:SetSize(300, 50);
+local targetLabel = Turbine.UI.Label();
+targetLabel:SetParent( mainWindow );
+targetLabel:SetPosition( 10, 10 );
+targetLabel:SetSize( 300, 50 );
 
-Player = Turbine.Gameplay.LocalPlayer.GetInstance();
-pWall = Player.GetWallet();
+local Player = Turbine.Gameplay.LocalPlayer.GetInstance();
+local pWall = Player:GetWallet();
+local maxWall = pWall:GetSize();
 
-maxWall = pWall.GetSize();
-
-function updateMainWindowContent()
+local function updateMainWindowContent()
+    local wishedItems = { "Herbstfest-Medaille", "Zerfledderter Schatten" };
     local text = {};
 
-    local wishedItems = {}
-    wishedItems[1] = "Hero's Mark";
-    wishedItems[2] = "Token of Heroism";
-
-    local sizeWishedItems = #wishedItems;
-    local showStruct = {}
-    local text = {}
-
-    for i = 1, sizeWishedItems do
+    for i = 1, #wishedItems do
         local currentWishedItem = wishedItems[i];
         local found = false;
 
         for j = 1, maxWall do  
             local currentItem = pWall:GetItem(j);
+            -- Turbine.Shell.WriteLine(tostring(currentItem:GetName()));
             local currentItemName = tostring(currentItem:GetName());
-        
-            if (currentItemName == currentWishedItem) then
-                showStruct[1] = currentItemName;
-                showStruct[2] = tostring(currentItem:GetQuantity());
+            local currentItemQuantity = tostring(currentItem:GetQuantity());
+
+           if currentItemName == currentWishedItem then
+                text[i] = currentItemName .. ": " .. currentItemQuantity;
                 found = true;
                 break;
             end
         end
-        if found then
-            text[i] = showStruct[1] .. ": " .. showStruct[2];
-        else
-            text[i] = wishedItems[i] .. ": 0"
-        end     
+
+        if not found then
+            text[i] = wishedItems[i] .. ": nichts";
+        end
     end
-    targetLabel:SetText(text[1] .. "\n" .. text[2]);
+
+    -- Update the label with all the found items
+    targetLabel:SetText( table.concat(text, "\n") );
 end
 
-updateMainWindowContent()
-pWall.ItemAdded = function(sender, args)
+updateMainWindowContent();
+pWall.ItemAdded = function( sender, args )
     updateMainWindowContent();
 end
-pWall.ItemRemoved = function(sender, args)
+pWall.ItemRemoved = function( sender, args )
     updateMainWindowContent();
 end
-targetLabel.MouseDoubleClick = function(sender, args)
-    mainWindow:SetVisible(false)
+targetLabel.MouseDoubleClick = function( sender, args )
+    mainWindow:SetVisible( false );
 end
