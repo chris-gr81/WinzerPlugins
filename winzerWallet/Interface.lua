9282e0bceb846559859879766a51881d36013daa
chris <135366101+chris-gr81@users.noreply.github.com>
2024-10-28 18:45:06 +0100

    Implement sidebar and show/hide functionality
    Implemented in interface.lua

--- winzerWallet/Interface.lua
+++ winzerWallet/Interface.lua
@@ -1,18 +1,49 @@
 -- Interface.lua
 Interface = {}
 
-mainBackClr = Turbine.UI.Color(0, 0, 0)
+-- Button-Class
+PanelButton = class ( Turbine.UI.Button );
+
+function PanelButton:Constructor()
+    Turbine.UI.Button.Constructor( self );
+    self:SetBackColor ( Turbine.UI.Color (0.2, 0.2, 0.2));
+    self:SetSize ( 40, 30 );
+
+    self.MouseDown = function (sender, args)
+        sender:SetBackColor(Turbine.UI.Color (0.1, 0.1, 0.1));
+    end
+
+    self.MouseUp = function (sender, args)
+        sender:SetBackColor(Turbine.UI.Color (0.2, 0.2, 0.2));
+    end
+end
+
+-- Erzeugt einheitliches Fenster-Design
+Interface.setStandardDesign = function(designElement)
+    local mainBackClr = Turbine.UI.Color(0, 0, 0)
+    designElement:SetVisible(true)
+    designElement:SetBackColor(mainBackClr)
+    designElement:SetOpacity(0.6)
+end
+
+local innerPadding = 10
+
+local function viewHide() 
+    if (Interface.mainWindow:IsVisible()) then
+        return "hide"
+    else
+        return "show"
+    end
+end
 
 Interface.mainWindow = Turbine.UI.Window()
-Interface.mainWindow:SetVisible(true)
+Interface.setStandardDesign(Interface.mainWindow)
 Interface.mainWindow:SetSize(300, 40)
 Interface.mainWindow:SetPosition(900, 30)
-Interface.mainWindow:SetBackColor(mainBackClr)
-Interface.mainWindow:SetOpacity(0.6)
 
 Interface.targetLabel = Turbine.UI.Label()
 Interface.targetLabel:SetParent(Interface.mainWindow)
-Interface.targetLabel:SetPosition(10, 10)
+Interface.targetLabel:SetPosition(innerPadding, innerPadding)
 Interface.targetLabel:SetSize(300, 50)
 
 -- Funktion, um das Label-Textfeld zu aktualisieren
@@ -21,7 +52,21 @@ function Interface.updateLabelText(text)
     Interface.targetLabel:SetText(table.concat(text, "\n"))
 end
 
--- Event-Handler für Doppelklick zum Verstecken des Fensters
-Interface.targetLabel.MouseDoubleClick = function(sender, args)
-    Interface.mainWindow:SetVisible(false)
+Interface.presentMenu = Turbine.UI.Window()
+Interface.setStandardDesign(Interface.presentMenu)
+Interface.presentMenu:SetSize(50, 100);
+Interface.presentMenu:SetPosition(00, 300)
+
+Interface.mainSwitchButton = PanelButton()
+Interface.mainSwitchButton:SetParent(Interface.presentMenu)
+Interface.mainSwitchButton:SetVisible(true)
+Interface.mainSwitchButton:SetPosition (5, 5)
+Interface.mainSwitchButton:SetText(viewHide())
+
+Interface.mainSwitchButton.Click = function( sender, args )
+    local current = Interface.mainWindow:IsVisible()
+    Interface.mainWindow:SetVisible(not current)
+    Interface.mainSwitchButton:SetText(viewHide())
 end
+
+
